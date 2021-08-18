38f653b1ae2aa1714323c6f9fd067cf83a306bcc
Adhika Gunadarma <adhika.gunadarma@simasjiwa.co.id>
2021-08-18 15:45:39 +0700

    reupdate category n video file and add course model

---
+++ src/model/course.model.js
@@ -0,0 +1,26 @@
+
+module.exports = mongoose => {
+    var schema = mongoose.Schema(
+        {
+            courseName: String,
+            courseDescription: String,
+            courseThumbnail : String,
+            courseThumbnailName : String,
+            courseTrailerVideoFile : String,
+            courseTrailerVideoName : String,
+            courseTotalBought : Number,
+            courseMembership : Boolean,
+            coursePublished : Boolean,
+        },
+        { timestamps: true }
+    );
+
+    schema.method("toJSON", function () {
+        const { __v, _id, ...object } = this.toObject();
+        object.id = _id;
+        return object;
+    });
+
+    const Course = mongoose.model("course", schema);
+    return Course;
+};
