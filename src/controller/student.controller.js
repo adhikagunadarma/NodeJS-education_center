8dc410b93ec75bea17ce4ffec9efe6dda5136e84
Adhika Gunadarma <adhikagunadarma@gmail.com>
2021-08-22 14:46:52 +0700

    find 1 and all students that active

--- src/controller/student.controller.js
+++ src/controller/student.controller.js
@@ -43,3 +43,54 @@ exports.create = async(req, res) => {
             });
         });
 };
+
+// Retrieve all students from the database.
+exports.findAll = (req, res) => {
+    const name = req.query.studentName;
+    //only get active students for findall
+    var condition = name ? { studentName: { $regex: new RegExp(title), $options: "i" },studentStatus : 1  } : { studentStatus : 1};
+
+    Student.find(condition)
+        .then(data => {
+            res.send({
+                statusMessage: "Succeed at fetching all students",
+                statusCode: 0,
+                data: data
+            });
+        })
+        .catch(err => {
+            res.status(500).send({
+                statusMessage: err.message || "Some error occurred while retrieving the students.",
+                statusCode: -999,
+            });
+        });
+};
+
+// Find a single student with an id
+exports.findOne = (req, res) => {
+    const id = req.params.id;
+
+    Teacher.findById(id)
+        .then(data => {
+            if (!data)
+                res.status(404).send({
+                    statusMessage: "Not found student with id " + id,
+                    statusCode: -999,
+                });
+            else {
+                res.send({
+                    statusMessage: "Succeed at fetching student " + data.teacherName,
+                    statusCode: 0,
+                    data: data
+                });
+            }
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .send({
+                    statusMessage: "Error retrieving student with id=" + id,
+                    statusCode: -999,
+                });
+        });
+};
