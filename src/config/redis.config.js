ecf1aa73abde5aec7f683f11a7f3b156a7ea88ed
Adhika Gunadarma <adhikagunadarma@gmail.com>
2021-11-02 20:22:22 +0700

    redis config

---
+++ src/config/redis.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  port: 6379,
+  host: "127.0.0.1",
+  // port: process.env.REDIS_PORT, //16494
+  // host: process.env.REDIS_HOST, //redis-16494.c252.ap-southeast-1-1.ec2.cloud.redislabs.com
+  // auth_pass: process.env.REDIS_AUTH_SECRET, //oGhseZCJIjD4skwhRoKj66DVbbb8OlDY
+};
